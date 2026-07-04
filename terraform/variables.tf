287a2381f228620e79af7b17ceb325e7656851f4
Aman Sinha <amank494@gmail.com>
2026-07-04 21:20:01 +0530

    test: upgrade RDS from db.t4g.medium to db.r7g.large

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,7 +29,7 @@ variable "root_volume_size" {
 variable "db_instance_class" {
   description = "RDS instance class"
   type        = string
-  default     = "db.t4g.micro"
+  default     = "db.r7g.large"
 }
 
 variable "db_storage_gb" {
