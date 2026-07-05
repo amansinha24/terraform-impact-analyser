339bcb27ab8658584b3ed46f6aec69ed60de247d
Aman Sinha <amank494@gmail.com>
2026-07-05 10:56:57 +0530

    Upgrade RDS from db.t3.micro to db.r7g.xlarge

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,7 +29,7 @@ variable "root_volume_size" {
 variable "db_instance_class" {
   description = "RDS instance class"
   type        = string
-  default     = "db.t3.micro"
+  default     = "db.r7g.xlarge"
 }
 
 variable "db_storage_gb" {
